6045953f565f6081b68305073bfdc8ab15403916
faithonwings7 <faithonwings7@gmail.com>
2019-10-18 02:59:05 -0400

    Add files via upload

--- UnitConversion.java
+++ UnitConversion.java
@@ -15,7 +15,7 @@ public class UnitConversion {
         //User enter unit
         System.out.print("Select original unit of measure: 1 - millimeter 2 - centimeter  3 - meter  4 - kilometer  5 - inch  6 - foot  7 - yard  8 - mile");
         int begUnit = input.nextInt();
-        if (begUnit < 1 || begUnit > 8 ){
+        if (begUnit < 1 || begUnit > 8) {
             System.out.print("Invalid option");
             //how to loop back to question again?
         }
@@ -23,178 +23,209 @@ public class UnitConversion {
         //User enter unit to convert to
         System.out.print("Select unit of measure to convert to: 1 - millimeter 2 - centimeter  3 - meter  4 - kilometer  5 - inch  6 - foot  7 - yard  8 - mile");
         int convUnit = input.nextInt();
-        if (begUnit < 1 || begUnit > 8 ){
+        if (begUnit < 1 || begUnit > 8) {
             System.out.print("Invalid option");
             //how to loop back to question again?
         }
 
-        if (begUnit = 1 && convUnit = 2){
+    }
+
+    public static void convert(double begUnit, double convUnit, double begValue){
+
+        if (begUnit == 1 && convUnit == 1){
+            System.out.print(begValue);
+        }
+        else if(begUnit == 1 && convUnit == 2){
             double endValue = miliToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 3){
+        else if (begUnit == 1 && convUnit == 3){
             double endValue = miliToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 4){
+        else if (begUnit == 1 && convUnit == 4){
             double endValue = miliToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 5){
+        else if (begUnit == 1 && convUnit == 5){
             double endValue = miliToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 6){
+        else if (begUnit == 1 && convUnit == 6){
             double endValue = miliToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 7){
+        else if (begUnit == 1 && convUnit == 7){
             double endValue = miliToYard(double begUnit, double convUnit);
         }
-        else if (begUnit = 1 && convUnit = 8){
+        else if (begUnit == 1 && convUnit == 8){
             double endValue = miliToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 1){
+        else if (begUnit == 2 && convUnit == 1){
             double endValue = centiToMilli()double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 3){
+        else if (begUnit == 2 && convUnit == 2){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 2 && convUnit == 3){
             double endValue = centiToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 4){
+        else if (begUnit == 2 && convUnit == 4){
             double endValue = centiToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 5){
+        else if (begUnit == 2 && convUnit == 5){
             double endValue = centiToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 6){
+        else if (begUnit == 2 && convUnit == 6){
             double endValue = centiToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 7){
+        else if (begUnit == 2 && convUnit == 7){
             double endValue = centiToYard(double begUnit, double convUnit);
         }
-        else if (begUnit = 2 && convUnit = 8){
+        else if (begUnit == 2 && convUnit == 8){
             double endValue = centiToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 1){
+        else if (begUnit == 3 && convUnit == 1){
             double endValue = meterToMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 2){
+        else if (begUnit == 3 && convUnit == 2){
             double endValue = meterToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 4){
+        else if (begUnit == 3 && convUnit == 3){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 3 && convUnit == 4){
             double endValue = meterToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 5){
+        else if (begUnit == 3 && convUnit == 5){
             double endValue = meterToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 6){
+        else if (begUnit == 3 && convUnit == 6){
             double endValue = meterToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 7){
+        else if (begUnit == 3 && convUnit == 7){
             double endValue = meterToYard(double begUnit, double convUnit);
         }
-        else if (begUnit = 3 && convUnit = 8){
+        else if (begUnit == 3 && convUnit == 8){
             double endValue = meterToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 1){
+        else if (begUnit == 4 && convUnit == 1){
             double endValue = kiloToMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 2){
+        else if (begUnit == 4 && convUnit == 2){
             double endValue = kiloToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 3){
+        else if (begUnit == 4 && convUnit == 3){
             double endValue = kiloToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 5){
+        else if (begUnit == 4 && convUnit == 4){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 4 && convUnit == 5){
             double endValue = kiloToInches(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 6){
+        else if (begUnit == 4 && convUnit == 6){
             double endValue = kiloToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 7){
+        else if (begUnit == 4 && convUnit == 7){
             double endValue = kiloToYard(double begUnit, double convUnit);
         }
-        else if (begUnit = 4 && convUnit = 8){
+        else if (begUnit == 4 && convUnit == 8){
             double endValue = kiloToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 1){
+        else if (begUnit == 5 && convUnit == 1){
             double endValue = inchtoMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 2){
+        else if (begUnit == 5 && convUnit == 2){
             double endValue = inchToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 3){
+        else if (begUnit == 5 && convUnit == 3){
             double endValue = inchToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 4){
+        else if (begUnit == 5 && convUnit == 4){
             double endValue = inchToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 6){
+        else if (begUnit == 5 && convUnit == 5){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 5 && convUnit == 6){
             double endValue = inchToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 7){
+        else if (begUnit == 5 && convUnit == 7){
             double endValue = inchToYard()double begUnit, double convUnit);
         }
-        else if (begUnit = 5 && convUnit = 8){
+        else if (begUnit == 5 && convUnit == 8){
             double endValue = inchToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 1){
+        else if (begUnit == 6 && convUnit == 1){
             double endValue = feetToMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 2){
+        else if (begUnit == 6 && convUnit == 2){
             double endValue = feetToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 3){
+        else if (begUnit == 6 && convUnit == 3){
             double endValue = feetToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 4){
+        else if (begUnit == 6 && convUnit == 4){
             double endValue = feetToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 5){
+        else if (begUnit == 6 && convUnit == 5){
             double endValue = feetToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 7){
+        else if (begUnit == 6 && convUnit == 6){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 6 && convUnit == 7){
             double endValue = feetToYard(double begUnit, double convUnit);
         }
-        else if (begUnit = 6 && convUnit = 8){
+        else if (begUnit == 6 && convUnit == 8){
             double endValue = feetToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 1){
+        else if (begUnit == 7 && convUnit == 1){
             double endValue = yardToMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 2){
+        else if (begUnit == 7 && convUnit == 2){
             double endValue = yardToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 3){
+        else if (begUnit == 7 && convUnit == 3){
             double endValue = yardToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 4){
+        else if (begUnit == 7 && convUnit == 4){
             double endValue = yardToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 5){
+        else if (begUnit == 7 && convUnit == 5){
             double endValue = yardToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 6){
+        else if (begUnit == 7 && convUnit == 6){
             double endValue = yardToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 7 && convUnit = 8){
+        else if (begUnit == 7 && convUnit == 7){
+            System.out.print(begValue);
+        }
+        else if (begUnit == 7 && convUnit == 8){
             double endValue = yardToMile(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 1){
+        else if (begUnit == 8 && convUnit == 1){
             double endValue = mileToMilli(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 2){
+        else if (begUnit == 8 && convUnit == 2){
             double endValue = mileToCenti(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 3){
+        else if (begUnit == 8 && convUnit == 3){
             double endValue = mileToMeter(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 4){
+        else if (begUnit == 8 && convUnit == 4){
             double endValue = mileToKilo(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 5){
+        else if (begUnit == 8 && convUnit == 5){
             double endValue = mileToInch(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 6){
+        else if (begUnit == 8 && convUnit == 6){
             double endValue = mileToFeet(double begUnit, double convUnit);
         }
-        else if (begUnit = 8 && convUnit = 7){
+        else if (begUnit == 8 && convUnit == 7){
             double endValue = mileToYard(double begUnit, double convUnit);
+        }
+        else if (begUnit == 8 && convUnit == 8){
+            System.out.print(begValue);
+        }
+        else{
+            //what do we put here??
         }
     }
 
