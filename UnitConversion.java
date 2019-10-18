55b7c178a1208bbc52dbd53ba40099d3bf6ffbe1
faithonwings7 <faithonwings7@gmail.com>
2019-10-18 01:21:12 -0400

    Add files via upload

---
+++ UnitConversion.java
@@ -0,0 +1,258 @@
+import java.util.Scanner;
+
+public class UnitConversion {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    //Ask user to select metric or standard system
+
+    //User enter number
+        System.out.print("Enter the number to be converted: ");
+
+        String begValue = input.nextLine();
+
+    //User enter original unit of measure:
+        //User enter unit
+        System.out.print("Enter original unit of measure: ");
+        String  = input.nextLine();
+
+        //User enter unit to convert to
+
+    //User enter number to be converted
+
+    }
+
+    public static void units(){
+
+        //create variables for each unit of measure
+        double millimeter = 0;
+        double centimeter = 0;
+        double meter = 0;
+        double kilometer = 0;
+        double inch = 0;
+        double foot = 0;
+        double yard = 0;
+        double mile = 0;
+
+
+
+    }
+
+    /*********************************************************************
+     METRIC SYSTEM
+     **********************************************************************/
+
+    //millimeter conversions
+    public static double miliToCenti(){
+        int conversionValue =
+    }
+    public static double miliToMeter(){
+
+    }
+    public static double miliToKilo(){
+
+    }
+    public static double miliToInch(){
+
+    }
+    public static double miliToFeet(){
+
+    }
+    public static double miliToYard(){
+
+    }
+    public static double miliToMile(){
+
+    }
+
+
+    //centimeter conversion
+    public static double centiToMilli(){
+
+    }
+    public static double centiToMeter(){
+
+    }
+    public static double centiToKilo(){
+
+    }
+    public static double centiToInch(){
+
+    }
+    public static double centiToFeet(){
+
+    }
+    public static double centiToYard(){
+
+    }
+    public static double centiToMile(){
+
+    }
+
+    //meter conversion
+    public static double meterToMilli(){
+
+    }
+    public static double meterToCenti(){
+
+    }
+    public static double meterToKilo(){
+
+    }
+    public static double meterToInch(){
+
+    }
+    public static double meterToFeet(){
+
+    }
+    public static double meterToYard(){
+
+    }
+    public static double meterToMile(){
+
+    }
+
+    //kilo conversion
+    public static double kiloToMilli(){
+
+    }
+
+    public static double kiloToCenti(){
+
+    }
+
+    public static double kiloToMeter(){
+
+    }
+
+    public static double kiloToInches(){
+
+    }
+
+
+    public static double kiloToFeet(){
+
+    }
+
+    public static double kiloToYard(){
+
+    }
+
+    public static double kiloToMile(){
+
+    }
+
+    /******************************************************************
+     IMPERIAL SYSTEM
+     *******************************************************************/
+
+    //inch conversion
+    public static double inchtoMilli(){
+
+    }
+    public static double inchToCenti(){
+
+    }
+    public static double inchToMeter(){
+
+    }
+    public static double inchToKilo(){
+
+    }
+    public static double inchToFeet(){
+
+    }
+    public static double inchToYard(){
+
+    }
+    public static double inchToMile(){
+
+    }
+
+    //feet conversion
+    public static double feetToMilli(){
+
+    }
+    public static double feetToCenti(){
+
+    }
+    public static double feetToMeter(){
+
+    }
+    public static double feetToKilo(){
+
+    }
+    public static double feetToInch(){
+
+    }
+
+    public static double meterToYard(){
+
+    }
+    public static double meterToMile(){
+
+    }
+
+
+    //yard conversion
+    public static double yardToMilli(){
+
+    }
+
+    public static double yardToCenti(){
+
+    }
+
+    public static double yardToMeter(){
+
+    }
+
+    public static double yardToKilo(){
+
+    }
+
+    public static double yardToInch(){
+
+    }
+
+    public static double yardToFeet(){
+
+    }
+
+    public static double yardToMile(){
+
+    }
+
+
+    //mile conversion
+    public static double mileToMilli(){
+
+    }
+
+    public static double mileToCenti(){
+
+    }
+
+    public static double mileToMeter(){
+
+    }
+
+    public static double mileToKilo(){
+
+    }
+
+    public static double mileToInch(){
+
+    }
+
+    public static double mileToFeet(){
+
+    }
+
+    public static double mileToYard(){
+
+    }
+
+}
